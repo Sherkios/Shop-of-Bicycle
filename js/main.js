8b311d09a1cdf65d8363217e1135fd6a072a00f0
Sherkios <54083024+Sherkios@users.noreply.github.com>
2020-07-23 10:17:49 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,32 @@
+
+
+$(document).ready(function(){
+	$('.main-screen__menu-burger').click(function(){
+		$('.main-screen__menu-burger,.main-screen__item-menu,.main-screen__menu-items').toggleClass('active')
+	});
+
+	$('.slider').slick({
+		arrows:false,
+		dots:true,
+		infinite:false,
+		autoplay:false,
+		autoplaySpeed:5000,
+		draggable:false,
+		touchMove:false,
+		waitForAnimate:true,
+		accessibylity:false,
+		fade:true,
+		});
+});
+
+function ibg(){
+
+let ibg=document.querySelectorAll('.ibg');
+for (var i = 0; i < ibg.length; i++) {
+if(ibg[i].querySelector('img')){
+ibg[i].style.backgroundImage = 'url('+ibg[i].querySelector('img').getAttribute('src')+')';
+}
+}
+}
+
+ibg();
